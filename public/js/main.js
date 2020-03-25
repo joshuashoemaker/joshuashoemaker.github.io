90236f4ea7c9cdc236f0c12361a6c48ad322312f
Joshua Shoemaker <joshuashoemaker@outlook.com>
2020-03-24 22:35:37 -0500

    update

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,19 @@
-$('#meetMeButton').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 500);
-    return false;
-});
+$('#meetMeButton').click(function () {
+  $('html, body').animate({
+    scrollTop: $($(this).attr('href')).offset().top
+  }, 500)
+  return false
+})
 
-
-$('.project').click(function(){
-    let id = $(this).attr('id');
-    if(id === "monas-escape"){
-        window.open("http://www.github.com/joshuashoemaker/game-off-2016", '_blank');
-    }
-    else if(id === "brightScreen"){
-        window.open("http://www.github.com/joshuashoemaker/brightScreen", '_blank');
-    }
-    else if(id === "ferryman"){
-        window.open("http://www.github.com/joshuashoemaker/theferryman", '_blank');
-    }
-    else if(id === "blogEngine"){
-        window.open("http://www.github.com/joshuashoemaker/python-blog-engine", '_blank');
-    }
+$('.project').click(function () {
+  const id = $(this).attr('id')
+  if (id === 'shc') {
+    window.open('http://scotthowardconsulting.com', '_blank')
+  } else if (id === 'tutor') {
+    window.open('https://www.wyzant.com/Tutors/WebDevWithJoshua', '_blank')
+  } else if (id === 'ferryman') {
+    window.open('http://www.github.com/joshuashoemaker/theferryman', '_blank')
+  } else if (id === 'blogEngine') {
+    window.open('http://www.github.com/joshuashoemaker/python-blog-engine', '_blank')
+  }
 })
