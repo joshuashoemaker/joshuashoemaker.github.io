b1a327d746bca06661a1caee1b1eb81c9ba7e109
root <root@DESKTOP-41KP8ON.localdomain>
2020-04-22 20:38:34 -0500

    fix: buttons go somewhere now

--- public/js/main.js
+++ public/js/main.js
@@ -5,15 +5,14 @@ $('#meetMeButton').click(function () {
   return false
 })
 
-$('.project').click(function () {
+$('.project').click(function (e) {
+  e.preventDefault()
   const id = $(this).attr('id')
   if (id === 'shc') {
     window.open('http://scotthowardconsulting.com', '_blank')
   } else if (id === 'tutor') {
     window.open('https://www.wyzant.com/Tutors/WebDevWithJoshua', '_blank')
-  } else if (id === 'ferryman') {
-    window.open('http://www.github.com/joshuashoemaker/theferryman', '_blank')
-  } else if (id === 'blogEngine') {
-    window.open('http://www.github.com/joshuashoemaker/python-blog-engine', '_blank')
+  } else if (id === 'blog') {
+    window.open('https://medium.com/@JShoemakerDev', '_blank')
   }
 })
